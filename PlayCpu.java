ed9294e5dfae94fe27185726f1ebc71ca97e05aa
Caleb Hamon <hamoncal25@7hills.org>
2023-10-12 19:50:04 -0400

    Adding new class to help with guessing This way one class isn't filled with multiple methods spanning closer to a thousand lines

--- PlayCpu.java
+++ PlayCpu.java
@@ -1,8 +1,62 @@
-public class PlayCpu {
-    
-    public void FastCPUSetup() {
-    
+import java.util.Scanner;
 
+public class PlayCpu {
+
+    //Where guessing implementations is created for both fast and reg games
+    //This goes through from FastCPUColGuess to RegCPURowGuess
+    
+    public static int FastCPUColGuess() {
+    
+        Scanner scan = new Scanner(System.in);
+        int colGuess;
+        System.out.println("Place a guess!");s
+        do {
+        System.out.println("What Col Would you like to guess?");
+        colGuess = scan.nextInt();
+        }
+        while (!(colGuess == 1 || colGuess == 2 || colGuess == 3 || colGuess == 4 || colGuess == 5 || colGuess == 6 || colGuess == 7 || colGuess == 8));
+        scan.close();
+        return colGuess;
+    }
+
+    public static int FastCPURowGuess() {
+    
+        Scanner scan = new Scanner(System.in);
+        int rowGuess;
+        do {
+        System.out.println("What Row Would you like to guess?");
+        rowGuess = scan.nextInt();
+        }
+        while (!(rowGuess == 1 || rowGuess == 2 || rowGuess == 3 || rowGuess == 4 || rowGuess == 5 || rowGuess == 6 || rowGuess == 7 || rowGuess == 8));
+        scan.close();
+        return rowGuess; 
+    }
+
+    public static int RegCPUColGuess() {
+    
+        Scanner scan = new Scanner(System.in);
+        int colGuess;
+        System.out.println("Place a guess!");
+        do {
+        System.out.println("What Col Would you like to guess?");
+        colGuess = scan.nextInt();
+        }
+        while (!(colGuess == 1 || colGuess == 2 || colGuess == 3 || colGuess == 4 || colGuess == 5 || colGuess == 6 || colGuess == 7 || colGuess == 8 || colGuess == 9 || colGuess == 10));
+        scan.close();
+        return colGuess;
+    }
+
+    public static int RegCPURowGuess() {
+    
+        Scanner scan = new Scanner(System.in);
+        int rowGuess;
+        do {
+        System.out.println("What Row Would you like to guess?");
+        rowGuess = scan.nextInt();
+        }
+        while (!(rowGuess == 1 || rowGuess == 2 || rowGuess == 3 || rowGuess == 4 || rowGuess == 5 || rowGuess == 6 || rowGuess == 7 || rowGuess == 8 || rowGuess == 9 || rowGuess == 10));
+        scan.close();
+        return rowGuess; 
     }
     
 }
